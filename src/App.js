6e842242fda7f15ae65a3b6a008f9727dbf067fa
Franco Giuntoli <franco.giuntoli@gmail.com>
2021-05-04 17:24:00 +0200

    IT2 done

--- src/App.js
+++ src/App.js
@@ -1,21 +1,49 @@
+
+import React from 'react';
 import './App.css';
 import data from './contacts.json'
 
-function App() {
+
+let actors = []
+data.map((e)=>{
+  if(actors.length<5){
+    actors.push(e)
+  }
+})
+class App extends React.Component{
+
+  state = {
+    celebs: actors
+  }
+
+
+
+  handlerAddRandom =()=>{
+    let randomIndex = Math.floor(Math.random()*data.length)
+    let elem = data[randomIndex]
   
-  return (
-    <div className="App">
-      {
-        data.map((e,i)=>{
+
+    this.setState({
+      celebs: [elem, ...this.state.celebs]
+  })
+}
+  
+
+  render(){
+    const {celebs} = this.state
+    return (
+      <div className="App">
+        <button onClick={this.handlerAddRandom}>Add Random Contact</button>
+        {
+          celebs.map((e,i)=>{
             return <ul key={i}>
-                      <li><img src={ e.pictureUrl} width="10%"/>,{e.name} ,{e.popularity.toFixed(2)}</li>
+                      <li><img src={e.pictureUrl} width="10%"/>,{e.name} ,{e.popularity.toFixed(2)}</li>
                     </ul>
           })
         }
-    
-
     </div>
-  );
+    )
+  };
 }
 
 export default App;
