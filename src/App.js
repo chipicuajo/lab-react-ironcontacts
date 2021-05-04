d417aa39a9a26fac011f43d9b82d16be9b802c97
Franco Giuntoli <franco.giuntoli@gmail.com>
2021-05-04 16:56:25 +0200

    IT1 done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import data from './contacts.json'
 
 function App() {
+  
   return (
     <div className="App">
-      
+      {
+        data.map((e,i)=>{
+            return <ul key={i}>
+                      <li><img src={ e.pictureUrl} width="10%"/>,{e.name} ,{e.popularity.toFixed(2)}</li>
+                    </ul>
+          })
+        }
+    
+
     </div>
   );
 }
